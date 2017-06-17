23b099f46d3dcf1c96abc2edcea54812dab51f45
projcloud9 <projcloud9@163.com>
2017-06-17 17:39:15 +0800

    init

---
+++ src/main/java/xin/fallen/UsedVehPicTransfer/UsedVehPicTransferApplication.java
@@ -0,0 +1,12 @@
+package xin.fallen.UsedVehPicTransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsedVehPicTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsedVehPicTransferApplication.class, args);
+	}
+}
